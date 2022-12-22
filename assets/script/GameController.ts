1ca0b106bedceced0556ee6b7f7fcb1b1689f612
nope <mrphu2001@gmail.com>
2022-12-22 11:08:45 +0700

    add logic

--- assets/script/GameController.ts
+++ assets/script/GameController.ts
@@ -1,5 +1,5 @@
 import { _decorator, Component, instantiate, Node, Prefab, Vec3 } from "cc";
-import { Card } from "./gameObject/Card";
+import { Card } from './gameObject/Card';
 const { ccclass, property } = _decorator;
 
 @ccclass("GameController")
@@ -14,22 +14,67 @@ export class GameController extends Component {
   NUM_OF_ROW = 2;
   CARD_WITH = 110;
   CARD_HEIGHT = 130;
+  CARD_TYPE_LIST: number[] = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
+  cardList: Node[] = [];
 
+  oldCard: Card
   start() {
+    let count = 0;
+
     for (let i = 0; i < this.NUM_OF_COL; i++) {
       for (let j = 0; j < this.NUM_OF_ROW; j++) {
         let card = instantiate(this.cardPrefab);
 
-        card.getComponent(Card).setup();
+        //gan id card
+        let cardType = this.CARD_TYPE_LIST[count];
+        count++;
+
+        card.getComponent(Card).setup(cardType, (card: Card) => {
+          this.onTouchCard(card)
+        });
 
         let x = -250 + i * (this.CARD_WITH + 10);
         let y = 0 + j * (this.CARD_HEIGHT + 10);
 
         card.setPosition(new Vec3(x, y, 0));
         this.groupCard.addChild(card);
+        this.cardList.push(card);
       }
     }
   }
 
-  update(deltaTime: number) {}
+  private onTouchCard(card: Card) {
+    //dong card da mo
+    if (this.oldCard == card) {
+      card.handleCloseCard();
+      this.oldCard = null;
+    }
+    //chon card khac
+    else {
+      card.handleUpCard(() => {
+        if (this.oldCard == null) {
+          this.oldCard = card;
+        } else {
+          if (this.oldCard.cardType == card.cardType) {
+            let removeIndex1 = this.cardList.findIndex(item => item === this.oldCard.node);
+            this.cardList.splice(removeIndex1, 1);
+            this.oldCard.node.destroy();
+            let removeIndex = this.cardList.findIndex(item => item === card.node);
+            this.cardList.splice(removeIndex, 1);
+            card.node.destroy();
+            this.oldCard = null;
+            console.log(this.cardList)
+          }
+          else {
+            this.oldCard.handleCloseCard(() => {
+              this.oldCard = null;
+            });
+            card.handleCloseCard();
+          }
+        }
+      });
+    }
+
+  }
+  update(deltaTime: number) { }
 }
--- assets/script/gameObject/Card.ts
+++ assets/script/gameObject/Card.ts
@@ -17,16 +17,36 @@ export class Card extends Component {
   @property(Sprite)
   cardSpire: Sprite;
 
-  start() {}
+  cardType;
+  callBack;
 
-  update(deltaTime: number) {}
+  start() {
+    //add su kien click
+    this.node.on(Node.EventType.TOUCH_START, this.onTouchCard, this);
+  }
 
-  setup() {
+  //su kien click
+  onTouchCard() {
+    this.callBack(this);
+
+  }
+
+  update(deltaTime: number) { }
+
+  setup(cardType, callBack) {
+    //dat id cho card
+    //truyen nhuwng chua su dung
+    this.cardType = cardType;
+    //truyen nhuwng chua su dung
+    this.callBack = callBack;
+
+    //up khi setup
     this.cardSpire.spriteFrame = this.cardSpireFrame[0];
   }
 
-  handleCloseCard() {
+  handleCloseCard(finishCallBack = null) {
     let cardParent = this.cardSpire.node;
+
     tween(cardParent)
       .sequence(
         tween(cardParent).to(0.2, { scale: new Vec3(0, 1, 0) }),
@@ -37,8 +57,32 @@ export class Card extends Component {
 
         tween(cardParent).to(0.2, { scale: new Vec3(1, 1, 1) }),
 
-        tween(cardParent).delay(0.2)
+        tween(cardParent).delay(0.2),
+
+        tween(cardParent).call(() => {
+          finishCallBack ? finishCallBack() : null;
+        })
       )
       .start();
+  }
+
+  handleUpCard(finishCallBack) {
+    let cardParent = this.cardSpire.node;
+
+    tween(cardParent).sequence(
+      tween(cardParent).to(0.2, { scale: new Vec3(0, 1, 0) }),
+
+      tween(cardParent).call(() => {
+        this.cardSpire.spriteFrame = this.cardSpireFrame[this.cardType];
+      }),
+
+      tween(cardParent).to(0.2, { scale: new Vec3(1, 1, 1) }),
+
+      tween(cardParent).delay(0.2),
+
+      tween(cardParent).call(() => {
+        finishCallBack();
+      })
+    ).start();
   }
 }
